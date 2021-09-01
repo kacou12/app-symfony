eda0b067f14435f82652ba2db44088d4fbd84035
kacou12 <kacou585@gmail.com>
2021-09-01 09:04:43 +0000

    new dashboard

---
+++ src/Controller/Admin/ArticleCrudController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Article;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+// use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ArticleCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Article::class;
+    }
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
+    }
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            // ...
+            ->setPermission(Action::DELETE, 'ROLE_ADMIN')
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            // IdField::new('id'),
+            TextField::new('title', "Titre")->setRequired(true),
+            TextField::new('author', "auteur")->setRequired(true),
+            // TextareaField::new('content', "contenu")->setRequired(true),
+            // TextareaField::new('content')->setLabel('contenu')->onlyOnForms()->setFormType(CKEditorType::class),
+            TextareaField::new('content')->setLabel('contenu'),
+            AssociationField::new('categorie', "categorie")->hideOnForm(),
+
+            ArrayField::new('commentArticles')->hideOnForm()->hideOnIndex(),
+            AssociationField::new('commentArticles')->hideOnDetail()->hideOnIndex()->setRequired(true)->hideOnForm(),
+
+            Field::new('imageFile', "image du site")->setFormType(FileType::class)->hideOnIndex()->hideOnDetail(),
+            ImageField::new('image', 'image du site')->setBasePath('/uploads/article')->hideOnForm()->hideOnIndex(),
+
+        ];
+    }
+}
