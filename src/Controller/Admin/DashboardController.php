8be1dd7c25f8354ef24f2b08afab42f07eeba83a
kacou12 <kacou585@gmail.com>
2021-09-02 00:18:52 +0000

    need only remove oldFile and add new if necessary

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,12 +3,21 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Article;
+use App\Entity\Categorie;
+use App\Entity\CommentArticle;
+use App\Entity\CommentVideo;
+use App\Entity\User;
+use App\Entity\Video;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+ * @isGranted("ROLE_ADMIN")
+ */
 class DashboardController extends AbstractDashboardController
 {
     /**
@@ -28,7 +37,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Mes admins', 'fa fa-home', User::class)->setPermission('ROLE_ADMIN');
         yield MenuItem::linkToCrud('Mes articles', 'fa fa-home', Article::class);
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('mes categories', 'fa fa-home', Categorie::class);
+        yield MenuItem::linkToCrud('commentaires video', 'fa fa-home', CommentVideo::class);
+        yield MenuItem::linkToCrud('commentaires articles', 'fa fa-home', CommentArticle::class);
+        yield MenuItem::linkToCrud('mes videos', 'fa fa-home', Video::class);
     }
 }
